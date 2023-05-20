7aaba2a9ba2c47bee328092fab848e4ded2e3c07
arslan369 <arslan.akram76@yahoo.com>
2023-05-21 01:10:13 +0500

    new files and folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import useSite from 'hooks/use-site';
 import { getPaginatedPosts } from 'lib/posts';
 import { WebsiteJsonLd } from 'lib/json-ld';
-
+import Image from 'next/image'
 import Layout from 'components/Layout';
 import Header from 'components/Header';
 import Section from 'components/Section';
@@ -32,7 +32,6 @@ export default function Home({ posts, pagination }) {
           }}
         />
       </Header>
-
       <Section>
         <Container>
           <h2 className="sr-only">Posts</h2>
